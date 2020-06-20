c635a351b2a48628013f060ca56c274be6c87613
Vadym Kinchur <vkincur@gmail.com>
2020-06-20 06:26:53 +0300

    Add post deleting

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import (
     BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='create_post'),
     path('post/<int:pk>/update/', BlogUpdateView.as_view(), name='update_post'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from django.views.generic import (
     DetailView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -24,3 +26,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
